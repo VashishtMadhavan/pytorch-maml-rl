fc22f72df1dd57135cec8a08e2f20996b9e51373
Vashisht Madhavan <vashisht@uber.com>
2018-11-14 22:10:01 -0800

    fixing subproc vec env to work with pygame

--- maml_rl/envs/subproc_vec_env.py
+++ maml_rl/envs/subproc_vec_env.py
@@ -8,63 +8,76 @@ if is_py2:
 else:
     import queue as queue
 
-class EnvWorker(mp.Process):
-    def __init__(self, remote, env_fn, queue, lock):
-        super(EnvWorker, self).__init__()
-        self.remote = remote
-        self.env = env_fn()
-        self.queue = queue
-        self.lock = lock
-        self.task_id = None
-        self.done = False
+class CloudpickleWrapper(object):
+    def __init__(self, var):
+        """
+        Uses cloudpickle to serialize contents (otherwise multiprocessing tries to use pickle)
 
-    def empty_step(self):
-        observation = np.zeros(self.env.observation_space.shape,
-                               dtype=np.float32)
-        reward, done = 0.0, True
-        return observation, reward, done, {}
+        :param var: (Any) the variable you wish to wrap for pickling with cloudpickle
+        """
+        self.var = var
 
-    def try_reset(self):
-        with self.lock:
-            try:
-                self.task_id = self.queue.get(True)
-                self.done = (self.task_id is None)
-            except queue.Empty:
-                self.done = True
-        observation = (np.zeros(self.env.observation_space.shape,
-            dtype=np.float32) if self.done else self.env.reset())
-        return observation
+    def __getstate__(self):
+        return cloudpickle.dumps(self.var)
 
-    def run(self):
-        while True:
-            command, data = self.remote.recv()
-            if command == 'step':
-                observation, reward, done, info = (self.empty_step()
-                    if self.done else self.env.step(data))
-                if done and (not self.done):
-                    observation = self.try_reset()
-                self.remote.send((observation, reward, done, self.task_id, info))
-            elif command == 'reset':
-                observation = self.try_reset()
-                self.remote.send((observation, self.task_id))
-            elif command == 'reset_task':
-                self.env.unwrapped.reset_task(data)
-                self.remote.send(True)
-            elif command == 'close':
-                self.remote.close()
+    def __setstate__(self, obs):
+        self.var = pickle.loads(obs)
+
+def _worker(remote, parent_remote, env_fn_wrapper, queue, lock):
+    parent_remote.close()
+    env = env_fn_wrapper.var()
+    task_id = None
+    global_done = False
+
+    while True:
+        try:
+            cmd, data = remote.recv()
+            if cmd == 'step':
+                if global_done:
+                    observation, reward, done, info = np.zeros(env.observation_space.shape, dtype=np.float32), 0.0, True, {}
+                else:
+                    observation, reward, done, info = env.step(data)
+                if done and (not global_done):
+                    with lock:
+                        try:
+                            task_id = queue.get(True)
+                            global_done = (task_id is None)
+                        except queue.Empty:
+                            global_done = True
+                    observation = (np.zeros(env.observation_space.shape, dtype=np.float32) if global_done else env.reset())
+                remote.send((observation, reward, done, task_id, info))
+            elif cmd == 'reset':
+                with lock:
+                    try:
+                        task_id = queue.get(True)
+                        global_done = (task_id is None)
+                    except queue.Empty:
+                        global_done = True
+                    observation = (np.zeros(env.observation_space.shape, dtype=np.float32) if global_done else env.reset())
+                remote.send((observation, task_id))
+            elif cmd == 'reset_task':
+                env.unwrapped.reset_task(data)
+                remote.send(True)
+            elif cmd == 'render':
+                remote.send(env.render(*data[0], **data[1]))
+            elif cmd == 'close':
+                remote.close()
                 break
-            elif command == 'get_spaces':
-                self.remote.send((self.env.observation_space,
-                                 self.env.action_space))
+            elif cmd == 'get_spaces':
+                remote.send((env.observation_space, env.action_space))
             else:
-                raise NotImplementedError()
+                raise NotImplementedError
+        except EOFError:
+            break
+
 
 class SubprocVecEnv(gym.Env):
     def __init__(self, env_factory, queue):
         self.lock = mp.Lock()
-        self.remotes, self.work_remotes = zip(*[mp.Pipe() for _ in env_factory])
-        self.workers = [EnvWorker(remote, env_fn, queue, self.lock)
-            for (remote, env_fn) in zip(self.work_remotes, env_factory)]
+        n_envs = len(env_factory)
+        self.remotes, self.work_remotes = zip(*[mp.Pipe() for _ in range(n_envs)])
+        self.workers = [mp.Process(target=_worker, args=(work_remote, remote, CloudpickleWrapper(env_fn), queue, self.lock))
+                          for (work_remote, remote, env_fn) in zip(self.work_remotes, self.remotes, env_factory)]
         for worker in self.workers:
             worker.daemon = True
             worker.start()
