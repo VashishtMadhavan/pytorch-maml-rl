7f2944df5ca8e86797dc319cbbfb00c02cbacf6e
Vashisht Madhavan <vashisht@uber.com>
2019-02-20 15:35:42 -0800

    updating test custom script to have meta-learning

--- test_custom.py
+++ test_custom.py
@@ -5,6 +5,7 @@ import torch
 import argparse
 import imageio
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 import maml_rl.envs
 from maml_rl.policies import ConvLSTMPolicy, ConvCLSTMPolicy
@@ -43,7 +44,7 @@ def load_params(policy_path, env, device, clstm=False, cnn_type='nature'):
 
 def evaluate(env, policy, device, test_eps=10, greedy=False, render=False, random=False, record=False, clstm=False):
     num_actions = env.action_space.n; total_frames = []
-    ep_rews = []; ep_steps = []
+    ep_rews = []; ep_steps = []; second_ep_rews = []
     for t in tqdm(range(test_eps)):
         obs = env.reset(); done = False
         embed_tensor = torch.zeros(1, num_actions + 2).to(device=device)
@@ -69,30 +70,47 @@ def evaluate(env, policy, device, test_eps=10, greedy=False, render=False, rando
                 action = action_dist.sample().cpu().numpy()
 
             if random:
-                obs, rew, done, _ = env.step(env.action_space.sample())
+                obs, rew, done, info = env.step(env.action_space.sample())
             else:
-                obs, rew, done, _ = env.step(action[0])
+                obs, rew, done, info = env.step(action[0])
 
             embed_arr = np.zeros(num_actions + 2)
             embed_arr[action[0]] = 1.
             embed_arr[-2] = rew
             embed_arr[-1] = float(done)
             embed_tensor = torch.from_numpy(embed_arr[None]).float().to(device=device)
-
             total_rew += rew; tstep += 1
+        second_ep_rews.append(rew)
         ep_rews.append(total_rew); ep_steps.append(tstep)
     if record:
         frames = [t[:,:,-1] for t in total_frames]
         imageio.mimsave("movie.gif", frames)
-    return ep_rews, ep_steps
+    return ep_rews, ep_steps, second_ep_rews
  
 def main():
     args = parse_args()
     env = gym.make(args.env)
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     policy = load_params(args.checkpoint, env, device, clstm=args.clstm, cnn_type=args.cnn_type)
-    episode_rew, episode_steps = evaluate(env, policy, device, greedy=args.greedy, test_eps=args.test_eps,
+    episode_rew, episode_steps, second_ep_rew = evaluate(env, policy, device, greedy=args.greedy, test_eps=args.test_eps,
         render=args.render, random=args.random, record=args.record, clstm=args.clstm)
+
+    if 'v1' in env.spec.id:
+        episode_rew = np.array(episode_rew); second_ep_rew = np.array(second_ep_rew)
+        first_ep_rew = episode_rew - second_ep_rew
+        type_err = np.zeros(4)
+        type_err[0] = np.sum(episode_rew == 2) / len(episode_rew)
+        type_err[3] = np.sum(episode_rew == 0) / len(episode_rew)
+        type_err[1] = np.sum(np.clip(first_ep_rew - second_ep_rew, 0, 2)) / len(episode_rew)
+        type_err[2] = np.sum(np.clip(second_ep_rew - first_ep_rew, 0, 2)) / len(episode_rew)
+
+        plt.title("Return Distribution per Episode")
+        plt.ylabel("'%' of trajectories")
+        plt.xticks([1,2,3,4], ['BothRew', 'FirstRew', 'SecondRew', 'NoRew'])
+        plt.bar([1,2,3,4],type_err)
+        plt.savefig("error_bdown.png")
+        print("SecondMeanRew: {}".format(np.mean(second_ep_rew)))
 
     if args.save:
         batch = args.checkpoint.split('/')[-1].split('.')[0].split('-')[1]
