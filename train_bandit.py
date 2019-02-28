dee4497cc21809d0c96523efe60afeb488cf11d4
Vashisht Madhavan <vashisht@uber.com>
2019-02-27 20:21:29 -0800

    changing default hparams for bandit

--- train_bandit.py
+++ train_bandit.py
@@ -58,12 +58,12 @@ if __name__ == '__main__':
     parser.add_argument('--tau', type=float, default=0.3, help='discount factor for GAE')
     parser.add_argument('--vf_coef', type=float, default=0.05, help='value function coeff')
     parser.add_argument('--ent_coef', type=float, default=0.01, help='entropy bonus coeff')
-    parser.add_argument('--batch-size', type=int, default=1000, help='num episodes for gradient est.')
+    parser.add_argument('--batch-size', type=int, default=2500, help='num episodes for gradient est.')
     parser.add_argument('--train-iters', type=int, default=1000, help='training iterations')
 
     # Miscellaneous
     parser.add_argument('--outdir', type=str, default='bandit_debug')
-    parser.add_argument('--workers', type=int, default=4, help='num workers for traj sampling')
+    parser.add_argument('--workers', type=int, default=100, help='num workers for traj sampling')
     args = parser.parse_args()
 
     # Device
