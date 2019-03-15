1ffb1f2d1a798e28245b12921d80efaf2245805a
Vashisht Madhavan <vashisht@uber.com>
2019-03-15 14:17:31 -0700

    updating test script for grid env

--- test_grid.py
+++ test_grid.py
@@ -7,38 +7,49 @@ import imageio
 from tqdm import tqdm
 
 import maml_rl.envs
-from maml_rl.policies import FFPolicy
+from maml_rl.policies import FFPolicy, GRUPolicy
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--env", type=str, default='GridGame-v0')
     parser.add_argument("--test-eps", type=int, default=1000)
     parser.add_argument("--checkpoint", type=str)
+    parser.add_argument("--d", type=int, default=1)
+    parser.add_argument("--n", type=int, default=1)
+    parser.add_argument("--lstm", action="store_true")
     parser.add_argument("--render", action="store_true")
     parser.add_argument("--greedy", action="store_true")
     parser.add_argument("--save", action="store_true")
     return parser.parse_args()
 
-def load_params(policy_path, env, device):
-    policy = FFPolicy(env.observation_space.shape[0], env.action_space.n, D=1)
+def load_params(policy_path, env, device, lstm=False, D=1, N=1):
+    if lstm:
+        policy = GRUPolicy(env.observation_space.shape[0], env.action_space.n, lstm_size=64, D=D, N=N)
+    else:
+        policy = FFPolicy(env.observation_space.shape[0], env.action_space.n, D=D)
     policy.load_state_dict(torch.load(policy_path, 
     	map_location=device if device == 'cpu' else None))
     return policy
 
-def evaluate(env, policy, device, test_eps=10, greedy=False, render=False):
+def evaluate(env, policy, device, test_eps=10, greedy=False, render=False, lstm=False, D=1):
     nA = env.action_space.n; frames = []
     epR = []; epT = []; sepR = []
     for t in tqdm(range(test_eps)):
         obs = env.reset(); done = False
-        #e_tensor = torch.zeros(1, nA + 2).to(device=device)
-        #e_tensor[:, 0] = 1.
+        if lstm:
+            hx = torch.zeros(D, 1, 64).to(device=device)
+        e_tensor = torch.zeros(1, nA + 2).to(device=device)
+        e_tensor[:, 0] = 1.
         R = 0; T = 0
 
         while not done:
             frames.append(np.array(obs))
             if render: env.render()
             obs_tensor = torch.from_numpy(np.array(obs)[None]).to(device=device)
-            action_dist, value_tensor = policy(obs_tensor)
+            if lstm:
+                action_dist, value_tensor, hx = policy(obs_tensor, hx, e_tensor)
+            else:
+                action_dist, value_tensor = policy(obs_tensor)
 
             if greedy:
                 probs = action_dist.probs.detach().cpu().numpy()
@@ -47,8 +58,8 @@ def evaluate(env, policy, device, test_eps=10, greedy=False, render=False):
                 action = action_dist.sample().cpu().numpy()[0]
             obs, rew, done, info = env.step(action)
 
-            #e = np.zeros(nA + 2); e[action] = 1.; e[-2] = rew; e[-1] = term_flag
-            #e_tensor = torch.from_numpy(e[None]).float().to(device=device)
+            e = np.zeros(nA + 2); e[action] = 1.; e[-2] = rew; e[-1] = float(done)
+            e_tensor = torch.from_numpy(e[None]).float().to(device=device)
             R += rew; T += 1
         sepR.append(rew)
         epR.append(R); epT.append(T)
@@ -59,9 +70,9 @@ def main():
     env = gym.make(args.env)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    policy = load_params(args.checkpoint, env, device)
+    policy = load_params(args.checkpoint, env, device, lstm=args.lstm, D=args.d, N=args.n)
     episode_rew, episode_steps, second_ep_rew = evaluate(env, policy, device, greedy=args.greedy,
-        test_eps=args.test_eps, render=args.render)
+        test_eps=args.test_eps, render=args.render, lstm=args.lstm, D=args.d)
 
     if args.save:
         batch = args.checkpoint.split('/')[-1].split('.')[0].split('-')[1]
